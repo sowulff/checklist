b227c6af574968ff02732e5d58a101cfb148be35
sowulff <sowu99@gmail.com>
2021-12-28 15:15:33 +0100

    login signup swap

--- login.php
+++ login.php
@@ -1,12 +1,35 @@
-<?php require __DIR__ . '/views/header.php'; ?>
+ <?php require __DIR__ . '/views/header.php';  ?>
 
 
-<h1>Log in</h1>
 
-<form action="app/users/login.php" method="post">
-    <label for="email">email address:</label>
-    <input type="email" name="email" id="email">
-    <label for="password">password:</label>
-    <input type="password" name="password" id="password">
-    <button type="submit">Login</button>
-</form>
+ <article class="welocome-signup">
+     <div class="hero">
+         <h1>
+             <div class="big-welcome">Welcome</div>
+             <span class="small-to">to</span> myList.
+         </h1>
+         <p>
+             Welocome to myList, ipsum dolor sit amet, consectetur adipiscing
+             elit. Curabitur et est sed felis aliquet sollicitudin.
+         </p>
+     </div>
+     <div class="form-container">
+         <div class="log-in-container">
+             <h2>Log in</h2>
+             <span>Don't have an account? <a href="signup.php">Create Your Account</a> it takes less than a minute</span>
+
+             <form action="app/users/login.php" method="post" class="login-form">
+                 <label for="email">email address:</label>
+                 <input type="email" name="email" id="email">
+                 <label for="password">password:</label>
+                 <input type="password" name="password" id="password">
+                 <div class="login-forgot">
+                     <button type="submit" class="btn">Login</button>
+                     <a class="forgot" href="#">Forgot Password?</a>
+                 </div>
+             </form>
+         </div>
+     </div>
+ </article>
+
+ <?php require __DIR__ . '/views/footer.php'; ?>
--- signup.php
+++ signup.php
@@ -1,66 +1,42 @@
- <?php require __DIR__ . '/views/header.php';  ?>
+<?php require __DIR__ . '/views/header.php'; ?>
+<article class="welocome-signup">
+    <div class="hero">
+        <h1>
+            <div class="big-welcome">Welcome</div>
+            <span class="small-to">to</span> myList.
+        </h1>
+        <p>
+            Welocome to myList, ipsum dolor sit amet, consectetur adipiscing
+            elit. Curabitur et est sed felis aliquet sollicitudin.
+        </p>
+    </div>
+    <div class="sign-up-container">
+        <span>Use your email for registration</span>
+        <form action="app/users/register.php" method="post" enctype="multipart/form-data">
+
+            <div>
+                <label for="name">Name</label>
+                <input type="name" name="name" id="name" required>
+
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required>
+
+            </div>
+
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
+
+            </div>
+            <div>
+                <label for="password-confirm">Confirm password</label>
+                <input type="password" name="password-confirm" id="password-confirm" required>
+            </div>
 
 
 
- <article class="welocome-signup">
-     <div class="hero">
-         <h1>
-             <div class="big-welcome">Welcome</div>
-             <span class="small-to">to</span> myList.
-         </h1>
-         <p>
-             Welocome to myList, ipsum dolor sit amet, consectetur adipiscing
-             elit. Curabitur et est sed felis aliquet sollicitudin.
-         </p>
-     </div>
-     <div class="form-container">
-         <div class="log-in-container">
-             <h2>Log in</h2>
-             <span>Don't have an account? <a href="#">Create Your Account</a> it takes less than a minute</span>
-
-             <form action="app/users/login.php" method="post" class="login-form">
-                 <label for="email">email address:</label>
-                 <input type="email" name="email" id="email">
-                 <label for="password">password:</label>
-                 <input type="password" name="password" id="password">
-                 <div class="login-forgot">
-                     <button type="submit" class="btn">Login</button>
-                     <a class="forgot" href="#">Forgot Password?</a>
-                 </div>
-             </form>
-         </div>
-         <div class="sign-up-container">
-             <span>Use your email for registration</span>
-             <form action="app/users/register.php" method="post" enctype="multipart/form-data">
-
-                 <div>
-                     <label for="name">Name</label>
-                     <input type="name" name="name" id="name" required>
-
-                 </div>
-                 <div>
-                     <label for="email">Email</label>
-                     <input type="email" name="email" id="email" required>
-
-                 </div>
-
-                 <div>
-                     <label for="password">Password</label>
-                     <input type="password" name="password" id="password" required>
-
-                 </div>
-                 <div>
-                     <label for="password-confirm">Confirm password</label>
-                     <input type="password" name="password-confirm" id="password-confirm" required>
-                 </div>
-
-
-
-                 <button type="submit">Sign up</button>
-             </form>
-         </div>
-
-     </div>
- </article>
-
- <?php require __DIR__ . '/views/footer.php'; ?>
+            <button type="submit">Sign up</button>
+        </form>
+</article>
