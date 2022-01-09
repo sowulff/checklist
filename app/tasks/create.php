ba401293085f0afb37d7bf96801a472b8b1bd721
sowulff <sowu99@gmail.com>
2022-01-09 14:02:07 +0100

    default = NULL at completed_at

--- app/tasks/create.php
+++ app/tasks/create.php
@@ -17,15 +17,17 @@ if (isset($_POST['title'])) {
     $content = $_POST['content'];
     $title = trim($_POST['title']);
     $userId = $_SESSION['user']['id'];
+    $myNull = null;
 
 
-    $statement = $database->prepare('INSERT INTO tasks (list_id, user_id, title, deadline_at, content)
-    VALUES (:list_id, :user_id, :title, :deadline_at, :content)');
+    $statement = $database->prepare('INSERT INTO tasks (list_id, user_id, title, deadline_at, content, completed_at)
+    VALUES (:list_id, :user_id, :title, :deadline_at, :content, :completed_at)');
     $statement->bindParam(':list_id', $list_id, PDO::PARAM_INT);
     $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
     $statement->bindParam(':title', $title, PDO::PARAM_STR);
     $statement->bindParam(':deadline_at', $deadline_at, PDO::PARAM_STR);
     $statement->bindParam(':content', $content, PDO::PARAM_STR);
+    $statement->bindParam(':completed_at', $myNull, PDO::PARAM_NULL);
 
     $statement->execute();
 }
