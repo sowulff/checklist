42a1ab3df14178272aeec2ce618343af201db02e
sowulff <sowu99@gmail.com>
2022-01-13 14:28:42 +0100

    Update all-tasks.php

--- all-tasks.php
+++ all-tasks.php
@@ -25,24 +25,23 @@ if (isset($_POST['all-tasks'])) : ?>
                 <?php endforeach ?>
             </ul>
         </div>
-    <?php endif; ?>
-
-    <div class="task-container">
-        <h3>Completed</h3>
-        <ul>
-            <?php foreach ($tasks as $task) :
-                if ($task['completed_at'] !== null) : ?>
-                    <li>
-                        <form action="/app/tasks/not-completed.php" method="POST">
-                            <?= $task['title']; ?>
-                    </li>
-                    <div class="info-about-task show">
-                        <p>description: <?php echo $task['content'] ?></p>
-                        <p>completed: <?php echo $task['completed_at'] ?></p>
-                        <p class="belongs-to-list">list: <?php echo get_list_for_task($task['id'], $database) ?></p>
-                    </div>
-                <?php endif; ?>
-            <?php endforeach ?>
-        </ul>
+        <div class="task-container">
+            <h3>Completed</h3>
+            <ul>
+                <?php foreach ($tasks as $task) :
+                    if ($task['completed_at'] !== null) : ?>
+                        <li>
+                            <form action="/app/tasks/not-completed.php" method="POST">
+                                <?= $task['title']; ?>
+                        </li>
+                        <div class="info-about-task show">
+                            <p>description: <?php echo $task['content'] ?></p>
+                            <p>completed: <?php echo $task['completed_at'] ?></p>
+                            <p class="belongs-to-list">list: <?php echo get_list_for_task($task['id'], $database) ?></p>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach ?>
+            </ul>
+        </div>
     </div>
-    </div>
+<?php endif; ?>
